6696213d7733d0f8b5d1624775af13edcc404b3f
Marc Mauri <marc@kaizenforpharma.com>
2020-12-09 18:21:22 +0100

    Sql Server specific mapping (money, smallmoney, was not covered in the generic mapping)

--- src/ClickHouseMigrator/SqlServer/SqlServerMigrator.cs
+++ src/ClickHouseMigrator/SqlServer/SqlServerMigrator.cs
@@ -13,14 +13,15 @@ namespace ClickHouseMigrator.SqlServer
 		{
 			using var conn = CreateDbConnection();
 			var primaryKeys = conn.Query($@"
-SELECT column_name as PRIMARYKEYCOLUMN
-FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
-INNER JOIN
-	INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
-		  ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND
-			 TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME AND
-			 KU.table_name = '{Options.SourceTable}'
-ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION; ").ToList();
+					SELECT column_name as PRIMARYKEYCOLUMN
+					FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
+					INNER JOIN
+						INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
+							  ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND
+								 TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME AND
+								 KU.table_name = '{Options.SourceTable}'
+					ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION;"
+				).ToList();
 
 			var columns = conn
 				.Query(
@@ -44,6 +45,82 @@ ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION; ").ToList();
 
 		protected override Lazy<string> ConnectionString => new Lazy<string>(() =>
 			$"Server=tcp:{Options.SourceHost},{Options.SourcePort};Initial Catalog={Options.SourceDatabase};Password={Options.SourcePassword};Persist Security Info=False;User ID={Options.SourceUser};MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;Connection Timeout=3000;");
+
+		protected override string ConvertToClickHouseDataType(string type)
+		{
+			var sizePrefixIndex = type.IndexOf('(');
+			var normalTypeName = sizePrefixIndex <= 0 ? type : type.Substring(0, sizePrefixIndex);
+			switch (normalTypeName.ToLower())
+			{
+				case "timestamp":
+				case "smalldatetime":
+				case "datetime2":
+				case "datetime":
+					{
+						return "DateTime";
+					}
+				case "date":
+					{
+						return "Date";
+					}
+				case "tinyint":
+					{
+						return "UInt8";
+					}
+				case "smallint":
+					{
+						return "Int16";
+					}
+				case "int":
+					{
+						return "Int32";
+					}
+				case "money":
+					{
+						return "Decimal64(4)";
+					}
+				case "smallmoney":
+					{
+						return "Decimal32(4)";
+					}
+				case "bit":
+					{
+						// Clickhouse has no Boolean
+						return "UInt8";
+					}
+				case "float":
+					{
+						return "Float64";
+					}
+				case "real":
+					{
+						return "Float32";
+					}
+				case "numeric":
+				case "decimal":
+					{
+						//todo get scale & precision as Float is incorrect because it is an approximation!
+						return "Float64";
+					}
+				case "bigint":
+					{
+						return "Int64";
+					}
+				case "uniqueidentifier":
+					{
+						return "UUID";
+					}
+				//case "char"/nchar:
+				//	{
+				//      TODO: must receive size
+				//		return "FixedString(n)
+				//	}
+				default:
+					{
+						return "String";
+					}
+			}
+		}
 
 		protected override IDbConnection CreateDbConnection()
 		{
